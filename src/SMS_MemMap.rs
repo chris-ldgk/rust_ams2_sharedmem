2223c2c86e359da2fda1158c552b01cc7289fd1a
Christoph Kolb <christoph.kolb@neugelb.com>
2022-12-13 18:37:14 +0100

    finalize memmap file

--- src/SMS_MemMap.rs
+++ src/SMS_MemMap.rs
@@ -1,32 +1,6 @@
-#![allow(non_camel_case_types)]
-
-/*************************************************************************************************
-  Description: 
-    Storage structure for storing and updating shared memory
-
-    Copyright (c) MWL. All rights reserved.
-*************************************************************************************************/
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// NOTES:
-// 
-//  -The shared memory variables will be updated once per graphics frame.
-//
-//  -Each variable comes with a UNIT, RANGE, and UNSET description where applicable.
-//     UNITS - Is the numeric form which a variable is stored in (e.g. KPH, Celsius)
-//     RANGE - Is the min-max ranges for a variable
-//     UNSET - Is the initialised/default/invalid value, depending on the variables usage
-//
-//  -Constant/unchanging values are included in the data, such as 'maxRPM', 'fuelCapacity' - this is done to allow percentage calculations.
-//
-//  -Also included are 12 unique enumerated types, to be used against the mentioned flag/state variables
-//
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+use std::os::raw::c_char;
 
 // *** Types ***
-
-use std::{os::raw::c_char};
-
 // Header version number to test against
 pub const SHARED_MEMORY_VERSION: usize = 13;
 // Maximum allowed length of string
@@ -37,40 +11,42 @@ pub const STORED_PARTICIPANTS_MAX: usize = 64;
 pub const TYRE_COMPOUND_NAME_LENGTH_MAX: usize = 40;
 
 // Tyres
+#[repr(usize)]
 pub enum TYRE_STATES {
-    TYRE_FRONT_LEFT = 1,
-    TYRE_FRONT_RIGHT = 2,
-    TYRE_REAR_LEFT = 3,
-    TYRE_REAR_RIGHT = 4,
+    TYRE_FRONT_LEFT = 0,
+    TYRE_FRONT_RIGHT,
+    TYRE_REAR_LEFT,
+    TYRE_REAR_RIGHT,
     //--------------
-    TYRE_MAX = 5
-} 
+    TYRE_MAX,
+}
 // Vector
+#[repr(usize)]
 pub enum VECTOR_AXES {
     VEC_X = 0,
-    VEC_Y = 1,
-    VEC_Z = 2,
+    VEC_Y,
+    VEC_Z,
     //-------------
-    VEC_MAX = 3
+    VEC_MAX,
 }
+
 // (Type#1) GameState (to be used with 'mGameState')
-pub enum GAME_STATES
-{
-    GAME_EXITED,
+pub enum GAME_STATES {
+    GAME_EXITED = 0,
     GAME_FRONT_END,
     GAME_INGAME_PLAYING,
     GAME_INGAME_PAUSED,
-        GAME_INGAME_INMENU_TIME_TICKING,
+    GAME_INGAME_INMENU_TIME_TICKING,
     GAME_INGAME_RESTARTING,
     GAME_INGAME_REPLAY,
     GAME_FRONT_END_REPLAY,
     //-------------
-    GAME_MAX
+    GAME_MAX,
 }
 
 // (Type#2) Session state (to be used with 'mSessionState')
 pub enum SESSION_STATES {
-    SESSION_INVALID,
+    SESSION_INVALID = 0,
     SESSION_PRACTICE,
     SESSION_TEST,
     SESSION_QUALIFY,
@@ -78,12 +54,12 @@ pub enum SESSION_STATES {
     SESSION_RACE,
     SESSION_TIME_ATTACK,
     //-------------
-    SESSION_MAX
+    SESSION_MAX,
 }
 
 // (Type#3) RaceState (to be used with 'mRaceState' and 'mRaceStates')
 pub enum RACE_STATES {
-    RACESTATE_INVALID,
+    RACESTATE_INVALID = 0,
     RACESTATE_NOT_STARTED,
     RACESTATE_RACING,
     RACESTATE_FINISHED,
@@ -91,25 +67,25 @@ pub enum RACE_STATES {
     RACESTATE_RETIRED,
     RACESTATE_DNF,
     //-------------
-    RACESTATE_MAX
+    RACESTATE_MAX,
 }
 
 // (Type#5) Flag Colours (to be used with 'mHighestFlagColour')
 pub enum FLAG_COLORS {
-    FLAG_COLOUR_NONE,             // Not used for actual flags, only for some query functions
-    FLAG_COLOUR_GREEN,                // End of danger zone, or race started
-    FLAG_COLOUR_BLUE,                 // Faster car wants to overtake the participant
-    FLAG_COLOUR_WHITE_SLOW_CAR,       // Slow car in area
-    FLAG_COLOUR_WHITE_FINAL_LAP,      // Final Lap
-    FLAG_COLOUR_RED,                  // Huge collisions where one or more cars become wrecked and block the track
-    FLAG_COLOUR_YELLOW,               // Danger on the racing surface itself
-    FLAG_COLOUR_DOUBLE_YELLOW,        // Danger that wholly or partly blocks the racing surface
-    FLAG_COLOUR_BLACK_AND_WHITE,      // Unsportsmanlike conduct
-    FLAG_COLOUR_BLACK_ORANGE_CIRCLE,  // Mechanical Failure
-    FLAG_COLOUR_BLACK,                // Participant disqualified
-    FLAG_COLOUR_CHEQUERED,            // Chequered flag
+    FLAG_COLOUR_NONE = 0, // Not used for actual flags, only for some query functions
+    FLAG_COLOUR_GREEN,    // End of danger zone, or race started
+    FLAG_COLOUR_BLUE,     // Faster car wants to overtake the participant
+    FLAG_COLOUR_WHITE_SLOW_CAR, // Slow car in area
+    FLAG_COLOUR_WHITE_FINAL_LAP, // Final Lap
+    FLAG_COLOUR_RED, // Huge collisions where one or more cars become wrecked and block the track
+    FLAG_COLOUR_YELLOW, // Danger on the racing surface itself
+    FLAG_COLOUR_DOUBLE_YELLOW, // Danger that wholly or partly blocks the racing surface
+    FLAG_COLOUR_BLACK_AND_WHITE, // Unsportsmanlike conduct
+    FLAG_COLOUR_BLACK_ORANGE_CIRCLE, // Mechanical Failure
+    FLAG_COLOUR_BLACK, // Participant disqualified
+    FLAG_COLOUR_CHEQUERED, // Chequered flag
     //-------------
-    FLAG_COLOUR_MAX
+    FLAG_COLOUR_MAX,
 }
 
 // (Type#6) Flag Reason (to be used with 'mHighestFlagReason')
@@ -119,56 +95,57 @@ pub enum FLAG_REASONS {
     FLAG_REASON_VEHICLE_CRASH,
     FLAG_REASON_VEHICLE_OBSTRUCTION,
     //-------------
-    FLAG_REASON_MAX
+    FLAG_REASON_MAX,
 }
 
 // (Type#7) Pit Mode (to be used with 'mPitMode')
+#[repr(usize)]
 pub enum PIT_MODES {
-    PIT_MODE_NONE,
+    PIT_MODE_NONE = 0,
     PIT_MODE_DRIVING_INTO_PITS,
     PIT_MODE_IN_PIT,
     PIT_MODE_DRIVING_OUT_OF_PITS,
     PIT_MODE_IN_GARAGE,
     PIT_MODE_DRIVING_OUT_OF_GARAGE,
     //-------------
-    PIT_MODE_MAX
+    PIT_MODE_MAX,
 }
 
 // (Type#8) Pit Stop Schedule (to be used with 'mPitSchedule')
 pub enum PIT_SCHEDULES {
-    PIT_SCHEDULE_NONE,            // Nothing scheduled
-    PIT_SCHEDULE_PLAYER_REQUESTED,    // Used for standard pit sequence - requested by player
-    PIT_SCHEDULE_ENGINEER_REQUESTED,  // Used for standard pit sequence - requested by engineer
-    PIT_SCHEDULE_DAMAGE_REQUESTED,    // Used for standard pit sequence - requested by engineer for damage
-    PIT_SCHEDULE_MANDATORY,           // Used for standard pit sequence - requested by engineer from career enforced lap number
-    PIT_SCHEDULE_DRIVE_THROUGH,       // Used for drive-through penalty
-    PIT_SCHEDULE_STOP_GO,             // Used for stop-go penalty
-    PIT_SCHEDULE_PITSPOT_OCCUPIED,    // Used for drive-through when pitspot is occupied
+    PIT_SCHEDULE_NONE = 0,           // Nothing scheduled
+    PIT_SCHEDULE_PLAYER_REQUESTED,   // Used for standard pit sequence - requested by player
+    PIT_SCHEDULE_ENGINEER_REQUESTED, // Used for standard pit sequence - requested by engineer
+    PIT_SCHEDULE_DAMAGE_REQUESTED, // Used for standard pit sequence - requested by engineer for damage
+    PIT_SCHEDULE_MANDATORY, // Used for standard pit sequence - requested by engineer from career enforced lap number
+    PIT_SCHEDULE_DRIVE_THROUGH, // Used for drive-through penalty
+    PIT_SCHEDULE_STOP_GO,   // Used for stop-go penalty
+    PIT_SCHEDULE_PITSPOT_OCCUPIED, // Used for drive-through when pitspot is occupied
     //-------------
-    PIT_SCHEDULE_MAX
+    PIT_SCHEDULE_MAX,
 }
 
 // (Type#9) Car Flags (to be used with 'mCarFlags')
 pub enum CAR_FLAGS {
-    CAR_HEADLIGHT         = (1<<0),
-    CAR_ENGINE_ACTIVE     = (1<<1),
-    CAR_ENGINE_WARNING    = (1<<2),
-    CAR_SPEED_LIMITER     = (1<<3),
-    CAR_ABS               = (1<<4),
-    CAR_HANDBRAKE         = (1<<5),
-    CAR_TCS               = (1<<6),
-    CAR_SCS               = (1<<7),
+    CAR_HEADLIGHT = (1 << 0),
+    CAR_ENGINE_ACTIVE = (1 << 1),
+    CAR_ENGINE_WARNING = (1 << 2),
+    CAR_SPEED_LIMITER = (1 << 3),
+    CAR_ABS = (1 << 4),
+    CAR_HANDBRAKE = (1 << 5),
+    CAR_TCS = (1 << 6),
+    CAR_SCS = (1 << 7),
 }
 
 pub enum TYRE_FLAGS {
-    TYRE_ATTACHED         = (1<<0),
-    TYRE_INFLATED         = (1<<1),
-    TYRE_IS_ON_GROUND     = (1<<2),
+    TYRE_ATTACHED = (1 << 0),
+    TYRE_INFLATED = (1 << 1),
+    TYRE_IS_ON_GROUND = (1 << 2),
 }
 
 // (Type#11) Terrain Materials (to be used with 'mTerrain')
 pub enum TERRAIN_MATERIALS {
-    TERRAIN_ROAD,
+    TERRAIN_ROAD = 0,
     TERRAIN_LOW_GRIP_ROAD,
     TERRAIN_BUMPY_ROAD1,
     TERRAIN_BUMPY_ROAD2,
@@ -216,11 +193,11 @@ pub enum TERRAIN_MATERIALS {
     TERRAIN_ICE_ROAD,
     TERRAIN_RUNOFF_ROAD,
     TERRAIN_ILLEGAL_STRIP,
-	TERRAIN_PAINT_CONCRETE,
-	TERRAIN_PAINT_CONCRETE_ILLEGAL,
-	TERRAIN_RALLY_TARMAC,
+    TERRAIN_PAINT_CONCRETE,
+    TERRAIN_PAINT_CONCRETE_ILLEGAL,
+    TERRAIN_RALLY_TARMAC,
     //-------------
-    TERRAIN_MAX
+    TERRAIN_MAX,
 }
 
 pub enum CRASH_STATES {
@@ -230,10 +207,8 @@ pub enum CRASH_STATES {
     CRASH_DAMAGE_SPINNING,
     CRASH_DAMAGE_ROLLING,
     //-------------
-    CRASH_MAX
+    CRASH_MAX,
 }
-
-
 
 // (Type#13) ParticipantInfo struct  (to be used with 'mParticipantInfo')
 #[derive(Debug)]
@@ -249,39 +224,37 @@ pub struct ParticipantInfo {
     pub mCurrentSector: i32,
 }
 
-
 pub enum DRS_STATES {
-	DRS_INSTALLED       = (1<<0),  // Vehicle has DRS capability
-	DRS_ZONE_RULES      = (1<<1),  // 1 if DRS uses F1 style rules
-	DRS_AVAILABLE_NEXT  = (1<<2),  // detection zone was triggered (only applies to f1 style rules)
-	DRS_AVAILABLE_NOW   = (1<<3),  // detection zone was triggered and we are now in the zone (only applies to f1 style rules)
-	DRS_ACTIVE          = (1<<4),  // Wing is in activated state
+    DRS_INSTALLED = (1 << 0),      // Vehicle has DRS capability
+    DRS_ZONE_RULES = (1 << 1),     // 1 if DRS uses F1 style rules
+    DRS_AVAILABLE_NEXT = (1 << 2), // detection zone was triggered (only applies to f1 style rules)
+    DRS_AVAILABLE_NOW = (1 << 3), // detection zone was triggered and we are now in the zone (only applies to f1 style rules)
+    DRS_ACTIVE = (1 << 4),        // Wing is in activated state
 }
 
 pub enum ERS_DEPLOYMENT_MODES {
-    ERS_DEPLOYMENT_MODE_NONE, // The vehicle does not support deployment modes
-    ERS_DEPLOYMENT_MODE_OFF, // Regen only, no deployment
+    ERS_DEPLOYMENT_MODE_NONE,  // The vehicle does not support deployment modes
+    ERS_DEPLOYMENT_MODE_OFF,   // Regen only, no deployment
     ERS_DEPLOYMENT_MODE_BUILD, // Heavy emphasis towards regen
     ERS_DEPLOYMENT_MODE_BALANCED, // Deployment map automatically adjusted to try and maintain target SoC
-    ERS_DEPLOYMENT_MODE_ATTACK,  // More aggressive deployment, no target SoC
-    ERS_DEPLOYMENT_MODE_QUAL, // Maximum deployment, no target Soc
+    ERS_DEPLOYMENT_MODE_ATTACK,   // More aggressive deployment, no target SoC
+    ERS_DEPLOYMENT_MODE_QUAL,     // Maximum deployment, no target Soc
 }
 
 // (Type#16) YellowFlagState represents current FCY state (to be used with 'mYellowFlagState')
-enum YellowFlagState
-{
-	YFS_INVALID = -1,
-	YFS_NONE,           // No yellow flag pending on track
-	YFS_PENDING,        // Flag has been thrown, but not yet taken by leader
-	YFS_PITS_CLOSED,    // Flag taken by leader, pits not yet open
-	YFS_PIT_LEAD_LAP,   // Those on the lead lap may pit
-	YFS_PITS_OPEN,      // Everyone may pit
-	YFS_PITS_OPEN2,     // Everyone may pit
-	YFS_LAST_LAP,       // On the last caution lap
-	YFS_RESUME,         // About to restart (pace car will duck out)
-	YFS_RACE_HALT,      // Safety car will lead field into pits
-	//-------------
-	YFS_MAXIMUM,
+enum YellowFlagState {
+    YFS_INVALID = -1,
+    YFS_NONE,         // No yellow flag pending on track
+    YFS_PENDING,      // Flag has been thrown, but not yet taken by leader
+    YFS_PITS_CLOSED,  // Flag taken by leader, pits not yet open
+    YFS_PIT_LEAD_LAP, // Those on the lead lap may pit
+    YFS_PITS_OPEN,    // Everyone may pit
+    YFS_PITS_OPEN2,   // Everyone may pit
+    YFS_LAST_LAP,     // On the last caution lap
+    YFS_RESUME,       // About to restart (pace car will duck out)
+    YFS_RACE_HALT,    // Safety car will lead field into pits
+    //-------------
+    YFS_MAXIMUM,
 }
 
 #[derive(Debug)]
@@ -354,6 +327,113 @@ pub struct SharedMemory {
     pub mLastOpponentCollisionIndex: f32,
     pub mBoostActive: bool,
     pub mBoostAmount: f32,
+
+    pub mOrientation: [f32; VECTOR_AXES::VEC_MAX as usize],
+    pub mLocalVelocity: [f32; VECTOR_AXES::VEC_MAX as usize],
+    pub mWorldVelocity: [f32; VECTOR_AXES::VEC_MAX as usize],
+    pub mAngularVelocity: [f32; VECTOR_AXES::VEC_MAX as usize],
+    pub mLocalAcceleration: [f32; VECTOR_AXES::VEC_MAX as usize],
+    pub mWorldAcceleration: [f32; VECTOR_AXES::VEC_MAX as usize],
+    pub mExtentsCentre: [f32; VECTOR_AXES::VEC_MAX as usize],
+
+    pub mTyreFlags: [u32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTerrain: [u32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreY: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreRPS: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreSlipSpeed: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreTemp: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreGrip: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreHeightAboveGround: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreLateralStiffness: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreWear: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mBrakeDamage: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mSuspensionDamage: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mBrakeTempCelsius: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreTreadTemp: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreLayerTemp: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreCarcassTemp: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreRimTemp: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreInternalAirTemp: [f32; TYRE_STATES::TYRE_MAX as usize],
+
+    pub mCrashState: u32,
+    pub mAeroDamage: f32,
+    pub mEngineDamage: f32,
+
+    pub mAmbientTemperature: f32,
+    pub mTrackTemperature: f32,
+    pub mRainDensity: f32,
+    pub mWindSpeed: f32,
+    pub mWindDirectionX: f32,
+    pub mWindDirectionY: f32,
+    pub mCloudBrightness: f32,
+
+    //PCars2 additions start, version 8
+    pub mSequenceNumber: u32,
+
+    pub mWheelLocalPositionY: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mSuspensionTravel: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mSuspensionVelocity: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mAirPressure: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mEngineSpeed: f32,
+    pub mEngineTorque: f32,
+    pub mWings: [f32; 2],
+    pub mHandbrake: f32,
+
+    pub mCurrentSector1Times: [f32; STORED_PARTICIPANTS_MAX],
+    pub mCurrentSector2Times: [f32; STORED_PARTICIPANTS_MAX],
+    pub mCurrentSector3Times: [f32; STORED_PARTICIPANTS_MAX],
+    pub mFastestSector1Times: [f32; STORED_PARTICIPANTS_MAX],
+    pub mFastestSector2Times: [f32; STORED_PARTICIPANTS_MAX],
+    pub mFastestSector3Times: [f32; STORED_PARTICIPANTS_MAX],
+    pub mFastestLapTimes: [f32; STORED_PARTICIPANTS_MAX],
+    pub mLastLapTimes: [f32; STORED_PARTICIPANTS_MAX],
+    pub mLapsInvalidated: [bool; STORED_PARTICIPANTS_MAX],
+    pub mRaceStates: [u32; STORED_PARTICIPANTS_MAX],
+    pub mPitModes: [u32; STORED_PARTICIPANTS_MAX],
+    pub mOrientations: [[f32; VECTOR_AXES::VEC_MAX as usize]; STORED_PARTICIPANTS_MAX],
+    pub mSpeeds: [f32; STORED_PARTICIPANTS_MAX],
+    pub mCarNames: [[c_char; STRING_LENGTH_MAX]; STORED_PARTICIPANTS_MAX],
+    pub mCarClassNames: [[c_char; STRING_LENGTH_MAX]; STORED_PARTICIPANTS_MAX],
+
+    pub mEnforcedPitStopLap: i32,
+    pub mTranslatedTrackLocation: [c_char; STRING_LENGTH_MAX],
+    pub mTranslatedTrackVariation: [c_char; STRING_LENGTH_MAX],
+    pub mBrakeBias: f32,
+    pub mTurboBoostPressure: f32,
+    pub mTyreCompound: [[c_char; TYRE_COMPOUND_NAME_LENGTH_MAX]; TYRE_STATES::TYRE_MAX as usize],
+    pub mPitSchedules: [u32; STORED_PARTICIPANTS_MAX],
+    pub mHighestFlagColours: [u32; STORED_PARTICIPANTS_MAX],
+    pub mHighestFlagReasons: [u32; STORED_PARTICIPANTS_MAX],
+    pub mNationalities: [u32; STORED_PARTICIPANTS_MAX],
+    pub mSnowDensity: f32,
+
+    // AMS2 Additions (v10...)
+    pub mSessionDuration: f32,
+    pub mSessionAdditionalLaps: i32,
+
+    pub mTyreTempLeft: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreTempCenter: [f32; TYRE_STATES::TYRE_MAX as usize],
+    pub mTyreTempRight: [f32; TYRE_STATES::TYRE_MAX as usize],
+
+    pub mDrsState: u32,
+
+    pub mRideHeight: [f32; TYRE_STATES::TYRE_MAX as usize],
+
+    pub mJoyPad0: u32,
+    pub mDPad: u32,
+
+    pub mAntiLockSetting: i32,
+    pub mTractionControlSetting: i32,
+
+    pub mErsDeploymentMode: i32,
+    pub mErsAutoModeEnabled: bool,
+
+    pub mClutchTemp: f32,
+    pub mClutchWear: f32,
+    pub mClutchOverheated: bool,
+    pub mClutchSlipping: bool,
+
+    pub mYellowFlagState: i32,
 }
 
 pub fn from_slice_u8_to_SharedMemory(slice: &[u8]) -> &SharedMemory {
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,50 @@
-use std::{mem::size_of, ffi::c_char};
+use std::{ffi::c_char, mem::size_of};
 mod SMS_MemMap;
-use SMS_MemMap::SharedMemory;
+use raw_sync::{events::*, Timeout};
 use shared_memory::ShmemConf;
+use std::thread::sleep;
+use std::time::Duration;
+use SMS_MemMap::SharedMemory;
 
-use crate::SMS_MemMap::from_slice_u8_to_SharedMemory;
+use crate::SMS_MemMap::{from_slice_u8_to_SharedMemory, SHARED_MEMORY_VERSION};
 
 fn zascii(slice: &[c_char]) -> String {
-    String::from_iter(slice.iter().take_while(|c| **c != 0).map(|c| *c as u8 as char))
-  }
+    String::from_iter(
+        slice
+            .iter()
+            .take_while(|c| **c != 0)
+            .map(|c| *c as u8 as char),
+    )
+}
 
 fn main() {
-    let shmem = match ShmemConf::new().os_id("$pcars2$").size(size_of::<SharedMemory>()).open() {
+    let shmem = match ShmemConf::new()
+        .os_id("$pcars2$")
+        .size(size_of::<SharedMemory>())
+        .open()
+    {
         Ok(m) => m,
-        Err(e) =>  {
-            eprintln!("Unable to create or open shmem: {}", e);
+        Err(e) => {
+            eprintln!(
+                "Unable to create or open shmem: {}. Is AMS2 running and Shared Memory enabled?",
+                e
+            );
             return;
         }
     };
 
-    unsafe {
-        println!("Is owner: {}", shmem.is_owner());
-        loop {
-            let slice = shmem.as_slice();
-            let shared_data: &SharedMemory = from_slice_u8_to_SharedMemory(slice);
-            println!("Game version: {}, Player ID: {:?}", shared_data.mVersion, zascii(&shared_data.mParticipantInfo[0].mName as &[i8]));
-            std::thread::sleep(std::time::Duration::from_secs(1));
-        }
+    // Loop version
+    loop {
+        let slice = unsafe { shmem.as_slice() };
+        let shared_data: &SharedMemory = from_slice_u8_to_SharedMemory(slice);
+        println!(
+            "Client version: {}\n
+            Game version: {}\n
+            Player ID: {}",
+            SHARED_MEMORY_VERSION,
+            shared_data.mVersion,
+            zascii(&shared_data.mParticipantInfo[0].mName as &[i8])
+        );
+        sleep(Duration::from_secs(1));
     }
 }
